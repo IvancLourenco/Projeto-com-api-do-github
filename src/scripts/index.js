27d0c6340ea49927660841b103f3fcabcf5b9813
unknown <ivancampos039@gmail.com>
2022-07-26 20:39:16 -0300

    novos arquivos de js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,24 @@ import{screen} from "/src/scripts/objects/screen.js"
 
 document.getElementById("btn-search").addEventListener("click", () =>{
     const userName = document.getElementById("input-search").value
+    if (validateEmptyInput(userName)) return
     getUserData(userName)
 })
+
+function validateEmptyInput(userName){
+    if(userName.length === 0){
+        alert("Preencha o campo com  o nome do usuário do Github")
+        return true
+    }
+}
 
 document.getElementById("input-search").addEventListener("keyup", (e) =>{
     const userName = e.target.value
     const key = e.which || e.keyCode
-    const isEnterKeyPressed =key === 13
+    const isEnterKeyPressed = key === 13
 
     if(isEnterKeyPressed){
+        if (validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
@@ -22,6 +31,12 @@ document.getElementById("input-search").addEventListener("keyup", (e) =>{
  async function getUserData(userName){
 
     const userResponse = await getUser(userName)
+    
+    if(userResponse.message === "Not Found"){
+        screen.renderNotFound()
+        return
+    }
+
     const repositoriesResponse = await getRepositories(userName)
 
     user.setInfo(userResponse)
